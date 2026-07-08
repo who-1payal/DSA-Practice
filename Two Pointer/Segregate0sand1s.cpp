3451600d6f6ad78de932c44be65b142a76556ded
Payal Sengar <psengar335@gmail.com>
2026-07-08 16:23:22 +0530

    Segregate 0s and 1s

--- Two Pointer/Segregate0sand1s.cpp
+++ Two Pointer/Segregate0sand1s.cpp
@@ -1,5 +1,32 @@
+/*Given an array arr[] consisting of only 0's and 1's. Modify the array in-place to segregate 0s onto the left side and 1s onto the right side of the array.
+
+Examples :
+Input: arr[] = [0, 1, 0, 1, 0, 0, 1, 1, 1, 0]
+Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+Explanation:  After segregation, all the 0's are on the left and 1's are on the right. Modified array will be [0, 0, 0, 0, 0, 1, 1, 1, 1, 1].
+
+Input: arr[] = [1, 1]
+Output: [1, 1]
+Explanation: There are no 0s in the given array, so the modified array is [1, 1]
+
+Constraints:
+1 ≤ arr.size() ≤ 105
+0 ≤ arr[i] ≤ 1 */
+
 #include <bits/stdc++.h>
 using namespace std;
+
+/*
+Approach:
+- Use two pointers.
+- Left pointer skips all 0s.
+- Right pointer skips all 1s.
+- Swap misplaced elements until the pointers meet.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 int main(){
     int n;
     cout << "Enter size of the array: ";
