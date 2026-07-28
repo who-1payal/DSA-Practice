cda69891a253ff287068ffca612cd86e0eed82d0
Payal Sengar <psengar335@gmail.com>
2026-07-29 00:19:36 +0530

    Merge Intervals

---
+++ Merge Intervals/MergeIntervals.cpp
@@ -0,0 +1,78 @@
+/*Merge Intervals
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+Example 1:
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+
+Example 2:
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Example 3:
+Input: intervals = [[4,7],[1,4]]
+Output: [[1,7]]
+Explanation: Intervals [1,4] and [4,7] are considered overlapping.
+ 
+Constraints:
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals){
+        sort(intervals.begin(),intervals.end());
+        int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
+        vector<vector<int>> res;
+
+        for(int i = 1; i<intervals.size();i++){
+            int start2 = intervals[i][0];
+            int end2 = intervals[i][1];
+
+            if(start2 <= end1){
+                end1 = max(end1,end2);
+            }
+            else{
+                res.push_back({start1,end1});
+                start1 = start2;
+                end1 = end2;
+            }
+
+        }
+        res.push_back({start1,end1});
+        return res;
+    }
+};
+
+int main(){
+
+    vector<vector<int>> a;
+    int start;
+    int end;
+    while(true){
+        cin>>start>>end;
+        if(start==-1 && end==-1){
+            break;
+        }
+        a.push_back({start,end});
+    }
+    
+    Solution obj;
+    vector<vector<int>> result = obj.merge(a);
+
+    cout << "Merged intervals:\n";
+
+    for (int i = 0; i < result.size(); i++) {
+        cout << "[" << result[i][0] << ", "
+             << result[i][1] << "] ";
+    }
+
+    return 0;
+}
