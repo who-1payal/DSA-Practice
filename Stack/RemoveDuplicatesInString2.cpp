7d8ebc844a18e8663f128d2d8afdff4e5db038a7
Payal Sengar <psengar335@gmail.com>
2026-08-16 23:04:48 +0530

    Remove All Adjacent Duplicates in String II

---
+++ Stack/RemoveDuplicatesInString2.cpp
@@ -0,0 +1,70 @@
+/* Remove All Adjacent Duplicates in String II
+You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+We repeatedly make k duplicate removals on s until we no longer can.
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+Example 1:
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+
+Example 2:
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+
+Example 3:
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+ 
+Constraints:
+1 <= s.length <= 105
+2 <= k <= 104
+s only contains lowercase English letters.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    string removeDuplicates(string s, int k){
+        stack<pair<char,int>> st;
+
+        for(int i = 0; i<s.size(); i++){
+            char ch = s[i];
+
+            if(!st.empty() && st.top().first == ch){
+                st.top().second++;
+
+                if(st.top().second == k){
+                    st.pop();
+                }
+            }
+            else{
+                st.push({ch,1});
+            }
+        }
+
+        string ans = "";
+        while(!st.empty()){
+            ans+= string(st.top().second,st.top().first);
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+int main(){
+    string s;
+    cout<<"Enter string: ";
+    cin>>s;
+    int k;
+    cout<<"Enter k: ";
+    cin>>k;
+    Solution sol;
+    cout<<sol.removeDuplicates(s,k);
+}
